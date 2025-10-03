bcb2e0d3adca32d6074e8b18fb9699013b2581dc
tulioumg <mlaras1@miumg.edu.gt>
2025-10-03 12:55:12 -0600

    Set initial failing state for FCC detection

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,8 +5,10 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNotNull(1, '1 is not null');
+      // Use assert.isNull() or assert.isNotNull() to make the tests pass. 
+      // isNull and isNotNull also take a string as the second parameter so you can have a custom error message.
+      assert.fail(null, 'This is an optional error description - e.g. null is null');
+      assert.fail(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
