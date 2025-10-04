6287512a0e72fed8be8248ce3259210a8c51c374
Tuliolara3002 <larasegur7@gmail.com>
2025-10-03 23:05:34 -0600

    Fix: Move Pug config immediately after app creation (like successful repo)

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
 
 app.route('/').get((req, res) => {
   res.render('index');
